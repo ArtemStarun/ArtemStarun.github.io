07dc765b0622d641354788ee2e6c9641dc5f68ef
ArtemStarun <89801617+ArtemStarun@users.noreply.github.com>
2021-11-20 14:00:54 +0300

    Подгрузил дз по jquery
    Дз выполнил в файле test.html, не получилось реализовать калькулятор в блоке прайса

--- js/myScript.js
+++ js/myScript.js
@@ -63,3 +63,12 @@ alert(`Стоимость создания сайта составит: ${summ}$
     }
 
 setTimeout (calk, 3000);
+
+$(document).ready(function(){
+    
+});
+
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 20 + "px"});
+});
